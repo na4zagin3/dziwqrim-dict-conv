c51e787f75a15bb784d507f64672f36a8e8973a1
na4zagin3 <mrty.ityt.pt@gmail.com>
2022-06-06 00:26:20 +0900

    Commit files that have not committed

---
+++ src/Lib/Kanji.hs
@@ -0,0 +1,154 @@
+{-# LANGUAGE OverloadedStrings #-}
+{-# LANGUAGE ImportQualifiedPost #-}
+module Lib.Kanji where
+
+import Data.Attoparsec.Text
+import Data.Attoparsec.Text qualified as AP
+import Data.Text (Text)
+import Data.Text qualified as T
+import Text.Printf (printf)
+
+ids2, ids3 :: String
+ids2 = "⿰⿱⿴⿵⿶⿷⿸⿹⿺⿻"
+ids3 = "⿲⿳"
+
+p_ids2 :: Parser Char
+p_ids2 = satisfy (`elem` ids2)
+
+p_ids3 :: Parser Char
+p_ids3 = satisfy (`elem` ids3)
+
+p_simpleKanji :: Parser Char
+p_simpleKanji = satisfy notIds
+  where
+    notIds = not . isIds
+    isIds = (`elem` (ids2 ++ ids3))
+
+p_kanji :: Parser Text
+p_kanji = choice [p_simpleKanji >>= return . T.singleton, p_seq2]
+  where
+    p_seq2 = do
+      op <- p_ids2
+      k1 <- p_kanji
+      k2 <- p_kanji
+      return $ mconcat [T.singleton op, k1, k2]
+
+explodeKanji :: Text -> Either String [Text]
+explodeKanji = parseOnly p
+  where
+    p = do
+      ks <- many' p_kanji
+      endOfInput
+      return ks
+
+p_book :: Parser Text
+p_book = do
+  string "《"
+  cs <- manyTill' anyChar (string "》")
+  return $ T.pack cs
+
+-- | Parse a fanqie
+--
+-- >>> parseTest p_fanqie "胡山反"
+-- Done "" ("\32993\23665","\21453")
+-- >>> parseTest p_fanqie "胡山切"
+-- Done "" ("\32993\23665","\20999")
+p_fanqie = do
+  f1 <- p_kanji
+  f2 <- p_kanji
+  fc <- choice [string "反", string "切"]
+  return (f1 <> f2, fc)
+
+p_paren s c e = do
+  string s
+  cs <- option "" c
+  string e <?> printf "end paren '%s'" e
+  return $ s <> cs <> e
+
+-- | Parse a balanced text
+--
+-- >>> parseOnly p_balancedText "abc"
+-- Right "abc"
+p_balancedText = T.concat <$> many' f
+  where
+    f = choice $
+        map (\(s, e) -> p_paren s p_balancedText e) parens
+        <> [AP.takeWhile1 (notInClass "（《“）》”")]
+    parens =
+      [ ("（", "）")
+      , ("《", "》")
+      , ("“", "”")
+      ]
+
+keyword_反切 :: [Text]
+keyword_反切 = ["未收", "脱字", "無本"]
+
+p_fanqieNote = do
+  k <- choice . map string $ keyword_反切
+  cp <- option "" $ do
+    string "（"
+    c <- p_balancedText
+    string "）"
+    return $ "（" <> c <> "）"
+  return $ k <> cp
+
+p_fanqieComment = do
+  string "（"
+  c <- p_balancedText
+  string "）"
+  return c
+
+-- | Parse a fanqie with an optional comment
+--
+-- >>> parseOnly p_fanqieWithComment "而鋭反（コメント）"
+-- Right (("\32780\37613","\21453"),Just "\12467\12513\12531\12488")
+-- >>> parseOnly p_fanqieWithComment "〓〓反（《王三》未收）"
+-- Right (("\12307\12307","\21453"),Just "\12298\29579\19977\12299\26410\25910")
+p_fanqieWithComment = do
+  fq <- p_fanqie
+  c <- option Nothing (Just <$> p_fanqieComment)
+  return $ (fq, c)
+
+p_fanqieItem = do
+  b <- option Nothing (Just <$> p_book)
+  (f, c) <- choice
+    [ p_fanqieComment >>= \c -> return (Nothing, Just c)
+    , p_fanqieNote >>= \c -> return (Nothing, Just c)
+    , p_fanqieWithComment >>= \(fq, c) -> return (Just fq, c)
+    ]
+  return (b, f, c)
+
+-- | Parse a fanqie field
+--
+-- >>> parseOnly p_fanqieField ""
+-- Right []
+-- >>> parseOnly p_fanqieField "なし"
+-- Right []
+-- >>> parseOnly p_fanqieField "（）"
+-- Right [(Nothing,Nothing,Just "")]
+-- >>> parseOnly p_fanqieField "（コメント）"
+-- Right [(Nothing,Nothing,Just "\12467\12513\12531\12488")]
+-- >>> parseOnly p_fanqieField "而鋭反（コメント）"
+-- Right [(Nothing,Just ("\32780\37613","\21453"),Just "\12467\12513\12531\12488")]
+-- >>> parseOnly p_fanqieField "〓〓反（《王三》未收）"
+-- Right [(Nothing,Just ("\12307\12307","\21453"),Just "\12298\29579\19977\12299\26410\25910")]
+-- >>> parseOnly p_fanqieField "《王三》未收"
+-- Right [(Just "\29579\19977",Nothing,Just "\26410\25910")]
+-- >>> parseOnly p_fanqieField "《王三》胡孟反"
+-- Right [(Just "\29579\19977",Just ("\32993\23391","\21453"),Nothing)]
+-- >>> parseOnly p_fanqieField "《王三》脱字（脱反語）"
+-- Right [(Just "\29579\19977",Nothing,Just "\33073\23383\65288\33073\21453\35486\65289")]
+-- >>> parseOnly p_fanqieField "《王一》户恢反，《王三》未收"
+-- Right [(Just "\29579\19968",Just ("\25143\24674","\21453"),Nothing),(Just "\29579\19977",Nothing,Just "\26410\25910")]
+-- >>> parseOnly p_fanqieField "《王一》户孟反，《王三》胡孟反"
+-- Right [(Just "\29579\19968",Just ("\25143\23391","\21453"),Nothing),(Just "\29579\19977",Just ("\32993\23391","\21453"),Nothing)]
+-- >>> parseOnly p_fanqieField "《王三》脱字（脱反語），《王一》無本"
+-- Right [(Just "\29579\19977",Nothing,Just "\33073\23383\65288\33073\21453\35486\65289"),(Just "\29579\19968",Nothing,Just "\28961\26412")]
+
+p_fanqieField
+  :: Parser [(Maybe Text, Maybe (Text, Text), Maybe Text)]
+p_fanqieField = choice
+  [ string "なし" *> return []
+  , p_fanqieItem `sepBy1` (char '，')
+  , string "" *> return []
+  ] <* endOfInput
---
+++ src/Lib/PhoneticRadical.hs
@@ -0,0 +1,50 @@
+{-# LANGUAGE OverloadedStrings #-}
+{-# LANGUAGE DeriveGeneric #-}
+{-# LANGUAGE ImportQualifiedPost #-}
+module Lib.PhoneticRadical where
+
+import Control.Arrow (left)
+import Control.Monad (join, liftM)
+import Data.Attoparsec.Text (parseOnly)
+import Data.List.NonEmpty (NonEmpty)
+import Data.List.NonEmpty qualified as NEL
+import Data.Map (Map)
+import Data.Map qualified as M
+import Data.Maybe (fromMaybe)
+import Data.Set (Set)
+import Data.Set qualified as S
+import Data.String (fromString)
+import Data.Text (Text)
+import Data.Text qualified as T
+import Data.Text.Read as TR
+import GHC.Generics (Generic)
+import GHC.Stack (HasCallStack)
+import Lib.Row (p_r_partNumber, maybeToRight)
+import Text.Printf (printf)
+
+data PhoneticRadical = PhoneticRadical
+  { pr_radical :: !Text
+  , pr_pronunciation :: ![Text]
+  , pr_index :: !(Int, Int)
+  , pr_comment :: !Text
+  }
+  deriving (Eq, Ord, Show, Read)
+
+p_pr_pronunciation :: Text -> [Text]
+p_pr_pronunciation "" = []
+p_pr_pronunciation "—" = []
+p_pr_pronunciation s = T.split (`T.elem` "\n") s
+
+parsePhoneticRadical :: (HasCallStack) => Int -> Map Text Text -> Either String PhoneticRadical
+parsePhoneticRadical row m = do
+  let lookupField f = maybeToRight f $ M.lookup (fromString f) m
+  f_pr_radical <- lookupField "聲首"
+  f_pr_index <- p_r_partNumber =<< lookupField "聲位"
+  f_pr_comment <- lookupField "コメント"
+  f_pr_pronunciation <- p_pr_pronunciation <$> lookupField "諧聲域"
+  return $ PhoneticRadical
+    { pr_radical = f_pr_radical
+    , pr_pronunciation = f_pr_pronunciation
+    , pr_index = f_pr_index
+    , pr_comment = f_pr_comment
+    }
