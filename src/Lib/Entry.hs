caee39fa795c5a43f917b6dd3b4f1bc46e60ed6d
na4zagin3 <mrty.ityt.pt@gmail.com>
2022-07-10 01:44:25 +0900

    Remove parentheses of character index at a part header

--- src/Lib/Entry.hs
+++ src/Lib/Entry.hs
@@ -463,10 +463,7 @@ unfoldTreeToTex isRoot t = fromMaybe "" (PT.root t) <> brackettedChildrenStr
       else "（" <> childrenStr <> "）"
 
 unfoldTreeToTex' :: (Ord a) => Bool -> PathTree a Text -> Text
-unfoldTreeToTex' isRoot t =
-    if null children || isRoot
-      then content
-      else "（" <> content <> "）"
+unfoldTreeToTex' isRoot t = content
   where
     children = M.toList $ PT.children t
     childrenStr = mconcat (map (unfoldTreeToTex' False . snd) $ children)
@@ -476,7 +473,7 @@ entriesToHeadingsTex :: (Ord a) => PathTree a [Entry] -> Text
 entriesToHeadingsTex pt = unfoldTreeToTex' True $ fmap renderEntries pt
   where
     renderEntries es = T.concat $ map renderEntry es
-    renderEntry e = "\\refEntry{" <> e_字 e <> "}{" <> entryToLabel e <> "}"
+    renderEntry e = "\\refEntry{" <> e_字 e <> "}{" <> entryToLabel e <> "} "
 
 sectionToTex :: Section -> Text
 sectionToTex s = mconcat
